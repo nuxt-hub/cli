3667dc18f2476a4dd546acabd5ccca079a4b810e
Sébastien Chopin <seb@nuxt.com>
2024-02-25 01:11:48 +0100

    fix: get correct git infos

--- src/utils/git.mjs
+++ src/utils/git.mjs
@@ -18,10 +18,10 @@ export function gitInfo() {
 
   if (isGitDir) {
     try {
-      git.dirty = Boolean(execSync('git status --porcelain', { stdio: 'ignore' }).toString().length)
-      git.branch = execSync('git rev-parse --abbrev-ref HEAD', { stdio: 'ignore' }).toString().trim()
-      git.commitHash = execSync('git rev-parse HEAD', { stdio: 'ignore' }).toString().trim()
-      git.commitMessage = execSync(`git show -s --format=%B ${git.commitHash}`, { stdio: 'ignore' }).toString().trim()
+      git.dirty = Boolean(execSync('git status --porcelain').toString().length)
+      git.branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+      git.commitHash = execSync('git rev-parse HEAD').toString().trim()
+      git.commitMessage = execSync(`git show -s --format=%B ${git.commitHash}`).toString().trim()
     } catch (err) {
       // Ignore
     }
