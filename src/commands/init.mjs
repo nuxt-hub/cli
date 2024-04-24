ea2a9e593a0c6f97b5e70a826cd1a14e1e6aced3
Jonathan Beckman <jonathantbeckman@gmail.com>
2024-04-24 06:04:59 -0400

    feat(deploy): support `--dotenv` arg

--- src/commands/init.mjs
+++ src/commands/init.mjs
@@ -4,7 +4,7 @@ import { execa } from 'execa'
 export default defineCommand({
   meta: {
     name: 'init',
-    description: 'Initialize a fresh NuxtHUb project, alias of `nuxi init -t hub`.',
+    description: 'Initialize a fresh NuxtHub project, alias of `nuxi init -t hub`.',
   },
   async setup({ args }) {
     await execa('npx', ['nuxi@latest', 'init', '-t', 'hub', ...args._], { stdio: 'inherit' })
