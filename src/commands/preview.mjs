4a805415b1f4c482e871d3f5f3093dbad9efa0e3
Sébastien Chopin <seb@nuxt.com>
2024-09-16 19:26:00 +0200

    feat: add `nuxthub preview` command

---
+++ src/commands/preview.mjs
@@ -0,0 +1,42 @@
+import { consola } from 'consola'
+import { defineCommand } from 'citty'
+import { writeFile, unlink } from 'node:fs/promises'
+import { join } from 'pathe'
+import { execa } from 'execa'
+import { existsSync } from 'fs'
+import { loadJsonFile } from 'load-json-file'
+import { generateWrangler } from '../utils/index.mjs'
+
+export default defineCommand({
+  meta: {
+    name: 'preview',
+    description: 'Preview your project locally (using `wrangler pages dev`).',
+  },
+  args: {},
+  async setup() {
+    const distDir = join(process.cwd(), 'dist')
+    // Read the dist/hub.config.json file
+    const hubConfig = await loadJsonFile(join(distDir, 'hub.config.json')).catch(() => null)
+    if (!existsSync(distDir) || !hubConfig) {
+      consola.error(`Production build not found, please run \`npx nuxt build\``)
+      process.exit(1)
+    }
+
+    consola.info('Generating wrangler.toml...')
+    const wrangler = generateWrangler(hubConfig)
+    const wranglerPath = join(distDir, 'wrangler.toml')
+    await writeFile(wranglerPath, wrangler)
+    consola.info('Starting `wrangler pages dev` command...')
+    const options = { stdio: 'inherit', cwd: distDir, preferLocal: true, localDir: process.cwd() }
+    await execa(options)`wrangler pages dev .`
+      .catch((err) => {
+        if (err.code === 'ENOENT') {
+          consola.error('`wrangler` is not installed, please make sure that you installed it with `npx nypm i -D wrangler`')
+          process.exit(1)
+        }
+        throw err
+      })
+    consola.info('Deleting generated wrangler.toml for preview...')
+    await unlink(wranglerPath)
+  },
+})
