1d5f5171ac552f5174184a1ff65db871c495d1d8
Rihan Arfan <me@file.properties>
2024-10-22 01:01:42 +0100

    fix: allow typing dashes in name

--- src/commands/database.mjs
+++ src/commands/database.mjs
@@ -25,9 +25,10 @@ const createMigration = defineCommand({
     const name = args.name
       .trim()
       .toLowerCase()
-      .replace(/[^a-zA-Z0-9\s\n]/g, '') // remove special characters except spaces and newlines
+      .replace(/[^a-zA-Z0-9\s\n-]/g, '') // remove special characters except spaces, newlines and dashes
       .replace(/[\s\n]+/g, '-') // replace spaces and newlines with dashes
       .replace(/^-+/, '') // remove leading and trailing dashes
+      .replace(/-+/g, '-') // replace multiple dashes with a single dash
       || 'migration'
     const migrationName = `${nextMigrationNumber}_${name}.sql`
     await useMigrationsStorage().set(migrationName, `-- Migration number: ${nextMigrationNumber} \t ${new Date().toISOString()}\n`)
