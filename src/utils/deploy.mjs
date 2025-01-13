71da17b7ea148c1c4741683f2420563f3f141e33
Rihan Arfan <me@file.properties>
2025-01-13 06:47:42 +0000

    feat: use sha256 for hash

--- src/utils/deploy.mjs
+++ src/utils/deploy.mjs
@@ -4,7 +4,7 @@ import { extname } from 'pathe'
 export function hashFile(filepath, base64) {
   const extension = extname(filepath).substring(1)
 
-  return createHash('sha1')
+  return createHash('sha256')
     .update(base64 + extension)
     .digest('hex')
     .slice(0, 32)
